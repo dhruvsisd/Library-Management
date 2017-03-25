45672c4d78bf6f626af7eb169b9a97a11a690388
Dhruv Aggarwal <dhruvsisd@gmail.com>
2017-03-25 22:36:23 +0530

    Add files via upload

---
+++ Project/src/PreparedStatement.java
@@ -0,0 +1,28 @@
+
+import java.sql.ResultSet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shivam
+ */
+class PreparedStatement {
+
+    void setInt(int i, int parseInt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    ResultSet executeQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
